9749ad5c2b65713d3fbc5fb39d5188101dd61864
AlyaAlmaz <aliaalmazrooie123@gmail.com>
2023-05-16 12:25:30 +0400

    added for ex

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("This is log")
+/*console.log("This is log")
+console.log(typeof numberOfStudents)
+var numberOfStudents = 25;
+console.log(typeof numberOfStudents)
+numberOfStudents = 99;
+console.log(typeof(typeof numberOfStudents))
+numberOfStudents = "fd"
+console.log(typeof numberOfStudents)
+
+
+
+var number = 50;
+console.log(typeof number)
+var nameof = "Alya";
+console.log(typeof nameof)
+var user = null;
+console.log(typeof user)*/
+
+var firstName = "Alya";
+var age = 23
+
+console.log(firstName , age)
+console.log(firstName.indexOf("A"))
+console.log(firstName.charAt(0))
+
+var longString = "Hello everyone iam alya";
+var isEndWith = longString.endsWith("iam alya");
+
+console.log("isEndWith:" , isEndWith)
+console.log("String length", firstName.length)
