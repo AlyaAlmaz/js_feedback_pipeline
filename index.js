0f6553949f5b459d6035fcfb449b989c423cfc2b
AlyaAlmaz <aliaalmazrooie123@gmail.com>
2023-05-16 16:02:14 +0400

    last update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ console.log(typeof number)
 var nameof = "Alya";
 console.log(typeof nameof)
 var user = null;
-console.log(typeof user)*/
+console.log(typeof user)
 
 var firstName = "Alya";
 var age = 23
@@ -28,3 +28,39 @@ var isEndWith = longString.endsWith("iam alya");
 
 console.log("isEndWith:" , isEndWith)
 console.log("String length", firstName.length)
+
+var toBeEvaluated = "\"Hello world! \" ";
+
+var EvaluatedValue = eval(toBeEvaluated);
+console.log(EvaluatedValue, typeof EvaluatedValue);
+
+ if(typeof(EvaluatedValue) == "String"){
+    var lastIndex = EvaluatedValue.length - 1;
+    console.log(EvaluatedValue.charAt(lastIndex))
+} 
+else{
+    var ValueToPrint = isNaN(EvaluatedValue)? "I cannot proceed" : (typeof ValueToPrint == "number")? oparseInt(EvaluatedValue) *3 : EvaluatedValue;
+    console.log(ValueToPrint)
+}
+
+var currentDay = "gfgfgff"
+
+switch(currentDay){
+    case "Saturday":
+        console.log("This is Saturday")
+        break;
+    case "Sunday":
+        console.log("Its okay you can sleep")
+        break;
+    case "Friday":
+        console.log("Only few hours left")
+        break;
+    default:
+        console.log("its time to work")
+        break;
+
+
+}*/
+for(var i = 0; i <=100 ; i+=2){
+    console.log(i)
+}
